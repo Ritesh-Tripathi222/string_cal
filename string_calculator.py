060a6e67103241c4363db2c3068a142e3bf6d92e
Ritesh <rt@surgepv.com>
2025-08-06 23:52:43 +0530

    Feat: Handle newlines

--- string_calculator.py
+++ string_calculator.py
@@ -2,5 +2,6 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if not numbers:
             return 0
+        numbers = numbers.replace('\n', ',')
         num_list = numbers.split(',')
         return sum(int(n) for n in num_list)
