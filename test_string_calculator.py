70af4290a3df4cc2dbe6a06735dfd1f43c0775fe
Ritesh <rt@surgepv.com>
2025-08-06 23:53:15 +0530

    Test: Add failing test for custom delimiters

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -20,3 +20,7 @@ def test_add_multiple_numbers():
 def test_add_new_lines():
     calculator = StringCalculator()
     assert calculator.add("1\n2,3") == 6
+
+def test_add_with_custom_delimiter():
+    calculator = StringCalculator()
+    assert calculator.add("//;\n1;2") == 3
