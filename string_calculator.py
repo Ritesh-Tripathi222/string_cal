b1eccac46540ed11d64260d1ef0b19fe2303497d
Ritesh <rt@surgepv.com>
2025-08-06 23:51:16 +0530

    Feat: Handle two numbers

--- string_calculator.py
+++ string_calculator.py
@@ -2,4 +2,5 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if not numbers:
             return 0
-        return int(numbers)
+        num_list = numbers.split(',')
+        return sum(int(n) for n in num_list)
