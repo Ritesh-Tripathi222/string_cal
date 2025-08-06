acf6ae99efc66310f8524ae05b9caf22cb106232
Ritesh <rt@surgepv.com>
2025-08-06 23:53:41 +0530

    Feat: Handle custom delimiters

--- string_calculator.py
+++ string_calculator.py
@@ -2,6 +2,12 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if not numbers:
             return 0
-        numbers = numbers.replace('\n', ',')
-        num_list = numbers.split(',')
-        return sum(int(n) for n in num_list)
+        delimiter = ','
+        if numbers.startswith('//'):
+            parts = numbers.split('\n', 1)
+            delimiter = parts[0][2:]
+            numbers = parts[1]
+
+        numbers = numbers.replace('\n', delimiter)
+        num_list = [int(n) for n in numbers.split(delimiter) if n]
+        return sum(num_list)
